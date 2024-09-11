53245cd65a4f10c5617687659a7bfc9f24287be3
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-09-11 11:34:30 +0330

    Update talkback.js

--- examplemods/talkback.js
+++ examplemods/talkback.js
@@ -3,12 +3,12 @@
         PluginAPI.addEventListener("processcommand", (event) => {
             if (event.command.toLowerCase().startsWith("/talkback")) {
                 var message = event.command.substring("/talkback ".length);
-                if (
+                /*if (
                     ModAPI.reflect.getClassById("net.minecraft.entity.player.EntityPlayerMP").instanceOf(event.sender)
-                ) {
+                ) {*/
                     event.sender.addChatMessage(ModAPI.reflect.getClassById("net.minecraft.util.ChatComponentText").constructors[0](ModAPI.util.str(message.toUpperCase())));
-                }
-                event.preventDefault = false;
+                //}
+                event.preventDefault = true;
             }
         });
     });
