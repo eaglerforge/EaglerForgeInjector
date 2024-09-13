310b73bdf91af9a4365d28d786071d9ce9f14124
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-09-13 12:38:02 +0330

    Update Worldedit.js

--- examplemods/Worldedit.js
+++ examplemods/Worldedit.js
@@ -20,10 +20,15 @@ ModAPI.addEventListener("lib:libcustomitems:loaded", () => {
         },
         onRightClickGround: `/*/user, world, itemstack, blockpos/*/
         const prefix = "§7[§4worldedit§7] ";
+        var username = user.getName();
 
-        globalThis.pos2x = blockpos.x
-        globalThis.pos2y = blockpos.y
-        globalThis.pos2z = blockpos.z
+        globalThis.pos2x = {}
+        globalThis.pos2y = {}
+        globalThis.pos2z = {}
+
+        globalThis.pos2x[username] = blockpos.x
+        globalThis.pos2y[username] = blockpos.y
+        globalThis.pos2z[username] = blockpos.z
         console.log("rightclick: " + blockpos.x + ", " + blockpos.y + ", " + blockpos.z)
         // Send chat message to player
         user.addChatMessage(ModAPI.reflect.getClassById("net.minecraft.util.ChatComponentText").constructors[0](ModAPI.util.str(prefix + "Pos #2 set to: " + blockpos.x + ", " + blockpos.y + ", " + blockpos.z)))
@@ -31,12 +36,16 @@ ModAPI.addEventListener("lib:libcustomitems:loaded", () => {
         `,
         onLeftClickGround: `/*/user, world, itemstack, blockpos/*/
         const prefix = "§7[§4worldedit§7] ";
+        var username = user.getName();
 
-        globalThis.posx = blockpos.x
-        globalThis.posy = blockpos.y
-        globalThis.posz = blockpos.z
+        globalThis.posx = {}
+        globalThis.posy = {}
+        globalThis.posz = {}
+
+        globalThis.posx[username] = blockpos.x
+        globalThis.posy[username] = blockpos.y
+        globalThis.posz[username] = blockpos.z
         
-        console.log("leftclick: " + blockpos.x + ", " + blockpos.y + ", " + blockpos.z)
         // Send chat message to player
         user.addChatMessage(ModAPI.reflect.getClassById("net.minecraft.util.ChatComponentText").constructors[0](ModAPI.util.str(prefix + "Pos #1 set to: " + blockpos.x + ", " + blockpos.y + ", " + blockpos.z)))
         return true;
@@ -84,11 +93,12 @@ ModAPI.addEventListener("lib:libcustomitems:loaded", () => {
             var blockPosConstructor = ModAPI.reflect.getClassById("net.minecraft.util.BlockPos").constructors.find((x) => { return x.length === 3 });
             if (event.command.toLowerCase().startsWith("//set")) {
                 const args = event.command.substring("//set ".length);
+                var username = event.sender.getName();
         
                 if (args) {
                     const blockTypeName = args
-                    const x1 = globalThis.posx, y1 = globalThis.posy, z1 = globalThis.posz;
-                    const x2 = globalThis.pos2x, y2 = globalThis.pos2y, z2 = globalThis.pos2z;
+                    const x1 = globalThis.posx[username], y1 = globalThis.posy[username], z1 = globalThis.posz[username];
+                    const x2 = globalThis.pos2x[username], y2 = globalThis.pos2y[username], z2 = globalThis.pos2z[username];
         
                     // Validate block and get block type
                     const blockType = ModAPI.blocks[blockTypeName];
