9fcac52d2866bc5218923db05f813b5296a7bfb0
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-10-02 14:49:17 +0800

    Docs for corrective, and patches stuff

--- patches.js
+++ patches.js
@@ -1,11 +1,17 @@
 class PatchesRegistry {
-    static patchedEventNames = []
+    static patchFns = []
     static patchedEventNames = []
     static getEventInjectorCode() {
-        return globalThis.modapi_specialevents = "[" + PatchesRegistry.patchedEventNames.flatMap(x=>`\`${x}\``).join(",") + "]"
+        return "globalThis.modapi_specialevents = [" + PatchesRegistry.patchedEventNames.flatMap(x=>`\`${x}\``).join(",") + "]"
+    }
+    static addPatch(fn) {
+        patchFns.push(fn);
+    }
+    static addSpecialEvent(x) {
+        PatchesRegistry.patchedEventNames.push(x);
     }
 }
-function addPatch(params) {
+function addPatch() {
     
 }
 function addSpecialEvent(eventName) {
