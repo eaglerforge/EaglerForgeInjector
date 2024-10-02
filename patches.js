4014518121011d638908cf41520077962e900166
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-10-02 00:34:47 -0700

    Update patches.js

--- patches.js
+++ patches.js
@@ -2,7 +2,7 @@ class PatchesRegistry {
     static patchFns = []
     static patchedEventNames = []
     static getEventInjectorCode() {
-        return "globalThis.modapi_specialevents = [" + PatchesRegistry.patchedEventNames.flatMap(x=>`\`${x}\``).join(",") + "];";
+        return "globalThis.modapi_specialevents = [" + PatchesRegistry.patchedEventNames.flatMap(x=>`\`${x}\``).join(",") + "]"
     }
     static patchFile(x) {
         var current = x;
@@ -23,3 +23,16 @@ class PatchesRegistry {
 //     var output = input;
 //     return output;
 // })
+
+PatchesRegistry.regSpecialEvent("render");
+PatchesRegistry.addPatch(function (input) {
+    var output = input.replaceAll(
+        /continue main;\s+?}\s+?if\s?\(!\$this.\$renderHand\)/gm
+        ,
+`continue main;
+            }
+            ModAPI.events.callEvent("render",{partialTicks:$partialTicks})
+            if (!$this.$renderHand)`
+    );
+    return output;
+})
