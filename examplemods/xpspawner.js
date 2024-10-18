2b73cd8f31a4ee9e355a193b63c564cabe1b80e0
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-10-18 18:58:05 +0800

    more comments

--- examplemods/xpspawner.js
+++ examplemods/xpspawner.js
@@ -1,34 +1,29 @@
 (() => {
-    PluginAPI.dedicatedServer.appendCode(function () {
-        PluginAPI.addEventListener("processcommand", (event) => {
+    PluginAPI.dedicatedServer.appendCode(function () { //Run this code on the server
+        PluginAPI.addEventListener("processcommand", (event) => { //Command processing event
             // Check if the sender is a player
             if (!ModAPI.reflect.getClassById("net.minecraft.entity.player.EntityPlayerMP").instanceOf(event.sender.getRef())) { return; }
 
-            // Check if the command is "/spawnsheep"
+            // Check if the command is "/spawnxp"
             if (event.command.toLowerCase().startsWith("/spawnxp")) {
                 const world = event.sender.getServerForPlayer();
                 const senderPos = event.sender.getPosition();
 
-                // Create a sheep entity
+                // Create an xp orb entity
                 const EntityXP = ModAPI.reflect.getClassByName("EntityXPOrb");
                 const xporb = EntityXP.constructors[0](world.getRef(), senderPos.getX(), senderPos.getY(), senderPos.getZ(), 10);
 
-                if (globalThis.AsyncSink) { //If we can, start the AsyncSink debugger to see filesystem requests
-                    AsyncSink.startDebuggingFS();
-                }
-
-                // Add the sheep to the world
+                // Add the xp orb to the world
                 ModAPI.promisify(ModAPI.hooks.methods.nmw_World_spawnEntityInWorld)(world.getRef(), xporb).then(result => {
-                    // Notify the player that the sheep has been spawned
-                    const ChatComponentTextClass = ModAPI.reflect.getClassById("net.minecraft.util.ChatComponentText");
-                    event.sender.addChatMessage(ChatComponentTextClass.constructors[0](ModAPI.util.str("An xp orb has been spawned!")));
+                    // Notify the player that the xp orb has been spawned
 
-                    if (globalThis.AsyncSink) { //Stop debugging when we are done, otherwise the console will get filled up.
-                        AsyncSink.stopDebuggingFS();
-                    }
+                    // Get the chat component class
+                    const ChatComponentTextClass = ModAPI.reflect.getClassById("net.minecraft.util.ChatComponentText");
+                    // Construct the chat component and send it to the client.
+                    event.sender.addChatMessage(ChatComponentTextClass.constructors[0](ModAPI.util.str("An xp orb has been spawned!")));
                 });
 
-                // Prevent the command from executing further
+                // Prevent the command from executing further (stops syntax errors / command does not exist errors)
                 event.preventDefault = true;
             }
         });
