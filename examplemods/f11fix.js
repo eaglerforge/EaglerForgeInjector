28f6946b3a6ce7cbc609cceba02d4fd8c9309f14
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-10-24 18:09:35 +0800

    add the semicolons

--- examplemods/f11fix.js
+++ examplemods/f11fix.js
@@ -4,9 +4,9 @@
     var oldF11 = HTMLElement.prototype.requestFullscreen;
     HTMLElement.prototype.requestFullscreen = function () {
         if (this instanceof HTMLBodyElement) {
-            oldF11.apply(this, [])
+            oldF11.apply(this, []);
         } else {
-            document.body.requestFullscreen()
+            document.body.requestFullscreen();
         }
     }
 })();
