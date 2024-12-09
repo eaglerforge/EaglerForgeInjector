b8fcdd3a6e2f8332a17b1c6d39d5258dd6ca26ad
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-12-09 11:26:13 +0800

    bump version

--- postinit.js
+++ postinit.js
@@ -1,4 +1,4 @@
-globalThis.ModAPIVersion = "v2.3.2";
+globalThis.ModAPIVersion = "v2.3.3";
 globalThis.modapi_postinit = "(" + (() => {
     //EaglerForge post initialization code.
     //This script cannot contain backticks, escape characters, or backslashes in order to inject into the dedicated server code.
