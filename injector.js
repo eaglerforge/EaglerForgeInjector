c336b3160ba2bf872b2f48dc348afb9e7a86da24
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-12-30 01:24:04 +0800

    bump ver

--- injector.js
+++ injector.js
@@ -1,4 +1,4 @@
-globalThis.ModAPIVersion = "v2.4";
+globalThis.ModAPIVersion = "v2.5";
 globalThis.doEaglerforge = true;
 document.querySelector("title").innerText = `EaglerForge Injector ${ModAPIVersion}`;
 document.querySelector("h1").innerText = `EaglerForge Injector ${ModAPIVersion}`;
