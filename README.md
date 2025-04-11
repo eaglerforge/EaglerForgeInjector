06a4480f47a9bbb85053d711ff60a18a451af449
BendieGamer <bendiereal@gmail.com>
2025-04-11 17:25:36 -0400

    Done!

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 # EaglerForgeInjector
+
 An advanced modding API injector for unminified, unobfuscated, unsigned eaglercraft builds.
 Current features:
+
 - Method hooking/monkey patching
 - Reflection
 - Custom classes
 - Modify the dedicated server
 
 ### How to use:
+
 #### Online
+
 Go to https://eaglerforge.github.io/EaglerForgeInjector/ and upload an unminified, unobfuscated, unsigned EaglercraftX offline download.
 
 #### Portable Offline
+
 Download this repository as a .zip, and extract it. Open index.html with your preferred browser (use `ctrl` + `O` on a new tab) and upload an unminified, unobfuscated, unsigned EaglercraftX offline download.
 
-#### Offline CLI
-- Clone the repository and run `npm ci` to install the required libraries.
-- In the project directory, run `npm run efi /help` for use instructions.
+#### Npx setup
+
+- Clone the repository and run `npm install -global eaglerforgeinjector` to install the required libraries.
+- In the project directory, run `npx efi help` or `npx efi` for use instructions.
 - This is good for bypassing browser memory limitations, for minifying.
 - Yes, I am forcing DOS command syntax upon you
 
 #### How does it work?
+
 This tool matches patterns in eaglercraft builds and adds patching code to let you modify how the code works at runtime. It then adds a [corelib](./postinit.js) that initialises the `ModAPI` object.
 
 #### History
+
 EaglerForgeInjector is a replacement for the `ModAPI` in the [old eaglerforge](https://github.com/EaglerForge/EaglerForge-old), which was maintained by @radmanplays. The legacy eaglerforge was a port of [Leah Anderson's EaglerReborn (dmca'd)](https://github.com/EaglerReborn/reborn)'s `PluginAPI` (created by me, @ZXMushroom63) to run on newer versions of Eaglercraft, with a few improvements and new features. Unlike EaglerForgeInjector, both legacy eaglerforge and eaglerreborn manually exposed properties and methods one by one.
 
 ## Discord server
+
 [https://discord.gg/rbxN7kby5W](https://discord.gg/rbxN7kby5W)
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+const { DOMParser } = require("@xmldom/xmldom");
+const fs = require("fs/promises");
+const fssync = require("fs");
+const path = require("path");
+const EFI = require("./core/core");
+
+async function main() {
+    const args = process.argv.slice(2);
+
+    if (args.includes("help") || args.length === 0) {
+        console.log("***************************");
+        console.log("* EaglerForgeInjector CLI *");
+        console.log("***************************");
+        console.log("");
+        console.log("> npx efi help #shows this help text");
+        console.log("> npx efi my_unminified_file.html #does nothing");
+        console.log("> npx efi my_unminified_file.html --eaglerforge #injects eaglerforge");
+        console.log("> npx efi my_unminified_file.html --minify #minifies file");
+        console.log("> npx efi my_unminified_file.html --minify --minify_extras #minifies file with extra optimisations");
+        console.log("> npx efi my_unminified_file.html --eaglerforge --minify #minifies file and injects eaglerforge");
+        console.log("> npx efi my_unminified_file.html --server_extras #inject server hosting stuff");
+        console.log("> npx efi my_unminified_file.html output.html --eaglerforge --minify #minifies file and injects eaglerforge, write to file named output.html");
+        console.log("> npx efi my_unminified_file.html --verbose #much logging");
+        console.log("By default, output is written to processed.html");
+        console.log("> npx efi init #Copies the template files");
+        return;
+    }
+
+
+    const inputFile = args[0];
+    if (!inputFile) {
+        console.log("***************************");
+        console.log("* EaglerForgeInjector CLI *");
+        console.log("***************************");
+        console.log("");
+        console.log("No file provided! Please provide a file to patch.");
+        console.log("Use 'npx efi help' for more information.");
+        return;
+    }
+
+    EFI.conf.doMinify = args.includes("--minify");
+    EFI.conf.doEaglerforge = args.includes("--eaglerforge");
+    EFI.conf.doServerExtras = args.includes("--server_extras");
+    EFI.conf.doMinifyPlus = args.includes("--minify_extras");
+    EFI.conf.verbose = args.includes("--verbose");
+
+    const string = await fs.readFile(inputFile, { encoding: 'utf-8' });
+    const res = await EFI.patchClient(string, new DOMParser());
+    if (res) {
+        var output = args[1];
+        if (!output || !output.endsWith(".html")) {
+            output = "processed.html";
+        }
+        console.log("Writing to " + output);
+        await fs.writeFile(output, res);
+        console.log("Done!");
+    }
+}
+
+main();
