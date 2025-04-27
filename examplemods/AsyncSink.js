8c0d5765e953e4cd31a0449293fbfbd6b7a66b96
Royan Xu <132798457+happylabdab2@users.noreply.github.com>
2025-04-26 17:10:17 -0700

    1.12 method correction
    1.8->1.12 method correction

--- examplemods/AsyncSink.js
+++ examplemods/AsyncSink.js
@@ -7,6 +7,13 @@ ModAPI.meta.credits("By ZXMushroom63");
     const ResourceLocation = ModAPI.reflect.getClassByName("ResourceLocation").constructors.find(x => x.length === 1);
     //AsyncSink is a plugin to debug and override asynchronous methods in EaglercraftX
     async function runtimeComponent() {
+        let booleanResult;
+        //jl_Boolean_valueOf-> 1.12, nlevit_BooleanResult__new-> 1.8 
+        if (ModAPI.is_1_12) {
+            booleanResult = (b) => ModAPI.hooks.methods.jl_Boolean_valueOf(b * 1);
+        } else {
+            booleanResult = (b) => ModAPI.hooks.methods.nlevit_BooleanResult__new(b * 1);
+        }
         const booleanResult = (b) => ModAPI.hooks.methods.nlevit_BooleanResult__new(b * 1);
         const wrap = ModAPI.hooks.methods.otji_JSWrapper_wrap;
         const unwrap = ModAPI.hooks.methods.otji_JSWrapper_unwrap;
